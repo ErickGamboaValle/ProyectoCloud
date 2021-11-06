208db903aad1b125301312f0c00974e4b8473ad9
Erick Gamboa Valle <is710115@iteso.mx>
2021-11-05 19:05:41 -0600

    Initial commit

---
+++ index.js
@@ -0,0 +1,19 @@
+const express = require('express');
+const path = require('path');
+
+
+let app = express();
+app.use(express.static(__dirname +'/public'));
+const port = process.env.PORT || 3000;
+
+app.listen(port, ()=>{
+    console.log('App is listening in port: ' + port);
+});
+
+app.get('/', function(req,res){
+    res.sendFile(path.resolve(__dirname, 'index.html'));
+});
+
+app.get('/api', function(req,res){
+    res.send('api works!');
+});
